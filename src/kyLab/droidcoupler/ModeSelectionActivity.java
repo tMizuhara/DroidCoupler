b0542103f55ff631e739341a2dca62c3cfed9bf8
Akihito Aiba <akihito1101@gmail.com>
2013-05-17 23:52:53 +0900

    add SendDataSettingActivity

--- src/kyLab/droidcoupler/ModeSelectionActivity.java
+++ src/kyLab/droidcoupler/ModeSelectionActivity.java
@@ -3,6 +3,8 @@ package kyLab.droidcoupler;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
 
 public class ModeSelectionActivity extends Activity {
 
@@ -17,6 +19,11 @@ public class ModeSelectionActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.mode_selection, menu);
 		return true;
+	}
+	
+	public void selectSendMode(View view){
+		Intent intent = new Intent(this, SendDataSettingActivity.class);
+		startActivity(intent);
 	}
 
 }
