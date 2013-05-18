b70499d26b3b1c5b368bb5c87ae60c9ba5f28139
koretaka <kore.kore.world@gmail.com>
2013-05-18 14:44:45 +0900

    change BinarySoundEncoder.java & SoundParam.java

--- src/kyLab/droidcoupler/BinarySoundEncoder.java
+++ src/kyLab/droidcoupler/BinarySoundEncoder.java
@@ -3,28 +3,26 @@ package kyLab.droidcoupler;
 import android.util.*;
 
 public class BinarySoundEncoder {
-	int SAMPLES_PER_BIT;
 	byte[] sinWave_s;		// start bit array
 	byte[] sinWave_e;		// end bit array
 	byte[] sinWave_0;		// 0 bit array
 	byte[] sinWave_1;		// 1 bit array
 
 	BinarySoundEncoder() {
-		SAMPLES_PER_BIT = SoundParam.SAMPLING_RATE / SoundParam.BPS;
-		sinWave_s = new byte[SAMPLES_PER_BIT];
-		sinWave_e = new byte[SAMPLES_PER_BIT];
-		sinWave_0 = new byte[SAMPLES_PER_BIT];
-		sinWave_1 = new byte[SAMPLES_PER_BIT];
+		sinWave_s = new byte[SoundParam.SAMPLES_PER_BIT];
+		sinWave_e = new byte[SoundParam.SAMPLES_PER_BIT];
+		sinWave_0 = new byte[SoundParam.SAMPLES_PER_BIT];
+		sinWave_1 = new byte[SoundParam.SAMPLES_PER_BIT];
 		
-		for (int i = 0; i < SAMPLES_PER_BIT; ++i) {
+		for (int i = 0; i < SoundParam.SAMPLES_PER_BIT; ++i) {
 			sinWave_s[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
-					* i * SoundParam.freq_s / SoundParam.SAMPLING_RATE));
+					* SoundParam.freq_s * i / SoundParam.SAMPLING_RATE));
 			sinWave_e[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
-					* i * SoundParam.freq_e / SoundParam.SAMPLING_RATE));
+					* SoundParam.freq_e * i / SoundParam.SAMPLING_RATE));
 			sinWave_0[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
-					* i * SoundParam.freq_0 / SoundParam.SAMPLING_RATE));
+					* SoundParam.freq_0 * i / SoundParam.SAMPLING_RATE));
 			sinWave_1[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
-					* i * SoundParam.freq_1 / SoundParam.SAMPLING_RATE));
+					* SoundParam.freq_1 * i / SoundParam.SAMPLING_RATE));
 		}
 	}
 
@@ -36,21 +34,23 @@ public class BinarySoundEncoder {
 		Log.d("encode", "data = " + data);
 
 		// start bit
-		System.arraycopy(sinWave_s, 0, sound, t, SAMPLES_PER_BIT);
-		t += SAMPLES_PER_BIT;
+		System.arraycopy(sinWave_s, 0, sound, t, SoundParam.SAMPLES_PER_BIT);
+		t += SoundParam.SAMPLES_PER_BIT;
 		Log.d("encode", "[" + t + "]");
 		for (int bit = 0; bit < 8; ++bit) {
 			Log.d("encode", "[" + bit + "]" + (data >> bit));
 			if (((data >> bit) & 0x01) == 0x01) {
-				System.arraycopy(sinWave_1, 0, sound, t, SAMPLES_PER_BIT);
+				System.arraycopy(sinWave_1, 0, sound, t, SoundParam.SAMPLES_PER_BIT);
 			} else {
-				System.arraycopy(sinWave_0, 0, sound, t, SAMPLES_PER_BIT);
+				System.arraycopy(sinWave_0, 0, sound, t, SoundParam.SAMPLES_PER_BIT);
 			}
-			t += SAMPLES_PER_BIT;
+			t += SoundParam.SAMPLES_PER_BIT;
 			Log.d("encode", "[" + t + "]");
 		}
 		// stop bit
-		System.arraycopy(sinWave_e, 0, sound, t, SAMPLES_PER_BIT);
+		System.arraycopy(sinWave_e, 0, sound, t, SoundParam.SAMPLES_PER_BIT);
+		t += SoundParam.SAMPLES_PER_BIT;
+		System.arraycopy(sinWave_e, 0, sound, t, SoundParam.SAMPLES_PER_BIT);
 		Log.d("encode", "[" + t + "]");
 	}
 
--- src/kyLab/droidcoupler/SoundParam.java
+++ src/kyLab/droidcoupler/SoundParam.java
@@ -4,11 +4,18 @@ public class SoundParam {
 
 	public static int SAMPLING_RATE = 44100;
 	public static int BPS = 10;
+	// Sound samples per bit data
+	public static int SAMPLES_PER_BIT = SAMPLING_RATE / BPS;
+	// Bits per packet
+	public static int PACKET_BITS = 11;
+	// Number of samples per packet data
+	public static int PACKET_SIZE = PACKET_BITS * SAMPLES_PER_BIT;
 
-	// Half of period (2/f) should be integral divisions of sound window (s/bit).
-	public static double freq_s = 200;		// start bit[Hz]
+	// Half of period (2/f) should be integral divisions of sound window
+	// (s/bit).
+	public static double freq_s = 200; 		// start bit[Hz]
 	public static double freq_0 = 100;		// binary [Hz]
-	public static double freq_1 = 300;		// binary [Hz]
-	public static double freq_e = freq_1;	// end bit[Hz]
+	public static double freq_1 = 300; 		// binary [Hz]
+	public static double freq_e = freq_1; 	// end bit[Hz]
 
 }
