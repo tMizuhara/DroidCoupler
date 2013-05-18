e650809b9976f12392032938d600af0301abd46b
koretaka <kore.kore.world@gmail.com>
2013-05-18 11:54:34 +0900

    change BinarySoundEncoder.java

--- src/kyLab/droidcoupler/BinarySoundEncoder.java
+++ src/kyLab/droidcoupler/BinarySoundEncoder.java
@@ -3,49 +3,55 @@ package kyLab.droidcoupler;
 import android.util.*;
 
 public class BinarySoundEncoder {
+	int SAMPLES_PER_BIT;
+	byte[] sinWave_s;		// start bit array
+	byte[] sinWave_e;		// end bit array
+	byte[] sinWave_0;		// 0 bit array
+	byte[] sinWave_1;		// 1 bit array
 
 	BinarySoundEncoder() {
+		SAMPLES_PER_BIT = SoundParam.SAMPLING_RATE / SoundParam.BPS;
+		sinWave_s = new byte[SAMPLES_PER_BIT];
+		sinWave_e = new byte[SAMPLES_PER_BIT];
+		sinWave_0 = new byte[SAMPLES_PER_BIT];
+		sinWave_1 = new byte[SAMPLES_PER_BIT];
+		
+		for (int i = 0; i < SAMPLES_PER_BIT; ++i) {
+			sinWave_s[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
+					* i * SoundParam.freq_s / SoundParam.SAMPLING_RATE));
+			sinWave_e[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
+					* i * SoundParam.freq_e / SoundParam.SAMPLING_RATE));
+			sinWave_0[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
+					* i * SoundParam.freq_0 / SoundParam.SAMPLING_RATE));
+			sinWave_1[i] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
+					* i * SoundParam.freq_1 / SoundParam.SAMPLING_RATE));
+		}
 	}
 
 	//
-	// Byte to Sound data (byte[SAMPLING_RATE])
+	// Encode Byte data to Sound data (byte[SAMPLING_RATE])
 	//
 	public void encByteSound(byte data, byte[] sound) {
-		double fs = SoundParam.freq_s;
-		double fe = SoundParam.freq_e;
-		double f0 = SoundParam.freq_0;
-		double f1 = SoundParam.freq_1;
-		int L = SoundParam.SAMPLING_RATE;
-		int dL = SoundParam.SAMPLING_RATE / SoundParam.BPS;	// samples per bit
 		int t = 0;
-
 		Log.d("encode", "data = " + data);
 
 		// start bit
-		for (int i = 0; i < dL; ++i, ++t) {
-			sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * fs
-					/ L));
-		}
+		System.arraycopy(sinWave_s, 0, sound, t, SAMPLES_PER_BIT);
+		t += SAMPLES_PER_BIT;
+		Log.d("encode", "[" + t + "]");
 		for (int bit = 0; bit < 8; ++bit) {
 			Log.d("encode", "[" + bit + "]" + (data >> bit));
-
 			if (((data >> bit) & 0x01) == 0x01) {
-				for (int i = 0; i < dL; ++i, ++t) {
-					sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
-							* t * f1 / L));
-				}
+				System.arraycopy(sinWave_1, 0, sound, t, SAMPLES_PER_BIT);
 			} else {
-				for (int i = 0; i < dL; ++i, ++t) {
-					sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI
-							* t * f0 / L));
-				}
+				System.arraycopy(sinWave_0, 0, sound, t, SAMPLES_PER_BIT);
 			}
+			t += SAMPLES_PER_BIT;
+			Log.d("encode", "[" + t + "]");
 		}
 		// stop bit
-		for (int i = 0; i < dL; ++i, ++t) {
-			sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * fe
-					/ L));
-		}
+		System.arraycopy(sinWave_e, 0, sound, t, SAMPLES_PER_BIT);
+		Log.d("encode", "[" + t + "]");
 	}
 
 }
