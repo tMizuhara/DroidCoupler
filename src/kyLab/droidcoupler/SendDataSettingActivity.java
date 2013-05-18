4a3c64aeaa2537d8f4e17d3938d9b0e7c4f3891b
Akihito Aiba <akihito1101@gmail.com>
2013-05-18 15:02:00 +0900

    use ProgressDialog

--- src/kyLab/droidcoupler/SendDataSettingActivity.java
+++ src/kyLab/droidcoupler/SendDataSettingActivity.java
@@ -2,17 +2,19 @@ package kyLab.droidcoupler;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.support.v4.app.NavUtils;
 import android.widget.EditText;
 
-public class SendDataSettingActivity extends Activity {
-
-	public static final String EXTRA_SENDING_DATA = "kyLab.droidCoupler.SENDING_DATA";
-
+public class SendDataSettingActivity extends Activity implements Runnable{
+	
+	private static ProgressDialog sendProgressDialog;
+	private Thread thread;
+	private SoundMessageSender soundMessageSender;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,10 +57,23 @@ public class SendDataSettingActivity extends Activity {
 	}
 
 	public void sendText(View view) {
-		Intent intent = new Intent(this, SendingActivity.class);
-		EditText editText = (EditText)findViewById(R.id.editSendText);
-		byte[] sendingData = (editText.getText().toString()).getBytes();
-		intent.putExtra(EXTRA_SENDING_DATA, sendingData);
-		startActivity(intent);
+		sendProgressDialog = new ProgressDialog(this);
+		sendProgressDialog.setMessage("データ送信中...");
+		sendProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		sendProgressDialog.show();
+		
+		thread = new Thread(this);
+		thread.start();
 	}
+	
+	 public void run() {
+	        // 時間のかかる処理をここに記述。
+		 	EditText editText = (EditText)findViewById(R.id.editSendText);
+			byte[] sendingData = (editText.getText().toString()).getBytes();
+		 	soundMessageSender = new SoundMessageSender();
+			soundMessageSender.sendSoundMessage(sendingData);
+			
+			sendProgressDialog.dismiss();
+    		sendProgressDialog = null;
+	 }
 }
