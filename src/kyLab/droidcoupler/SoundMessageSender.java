71d5fff9ce3567a27362c4429f07eb63128beeba
Takuya Mizuhara <Takuya@Takuya-no-MacBook-Air.local>
2013-05-18 14:26:51 +0900

    Change mode_static to mode_stream

--- src/kyLab/droidcoupler/SoundMessageSender.java
+++ src/kyLab/droidcoupler/SoundMessageSender.java
@@ -8,27 +8,16 @@ import android.util.Log;
 public class SoundMessageSender {
 
 	private AudioTrack track = null;
-	private byte[] sendingData = { 1, 2, 3, 4, 5 };
 	private BinarySoundEncoder encoder;
-	
-	//private static int SAMPLING_RATE = 441000;
-	//private static int BPS = 10;
-	//private static int NUMBER_OF_SAMPLES = (int) (SAMPLING_RATE * (1.0 / BPS));
 
 	SoundMessageSender() {
 		Log.d("SoundMessageSender", "sampling_rate:" + SoundParam.SAMPLING_RATE);
-		Log.d("SoundMessageSender", "bps:" + SoundParam.BPS);
-		//Log.d("SoundMessageSender", "number_of_samples:" + SoundParam.NUMBER_OF_SAMPLES);
 		
 		if (track == null) {
-			// track = new AudioTrack(AudioManager.STREAM_MUSIC, 44100,
-			// AudioFormat.CHANNEL_CONFIGURATION_DEFAULT,
-			// AudioFormat.ENCODING_DEFAULT, 44100, AudioTrack.MODE_STATIC);
 			track = new AudioTrack(AudioManager.STREAM_MUSIC, SoundParam.SAMPLING_RATE,
 					AudioFormat.CHANNEL_OUT_DEFAULT,
-					AudioFormat.ENCODING_DEFAULT, SoundParam.SAMPLING_RATE, AudioTrack.MODE_STATIC);
+					AudioFormat.ENCODING_DEFAULT, SoundParam.SAMPLING_RATE, AudioTrack.MODE_STREAM);
 		}
-		
 		encoder = new BinarySoundEncoder();
 	}
 
