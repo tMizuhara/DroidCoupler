dd04cfd9bc01dc5d4cd763a5a0c4d69f76c612c0
koretaka <kore.kore.world@gmail.com>
2013-05-18 10:06:32 +0900

    Add SoundParam.java Change BinarySoundEncoder.java

--- src/kyLab/droidcoupler/BinarySoundEncoder.java
+++ src/kyLab/droidcoupler/BinarySoundEncoder.java
@@ -5,39 +5,39 @@ import android.util.*;
 public class BinarySoundEncoder {
 
 	//
-	// Byte to Sound data (byte[length = 44100])
+	// Byte to Sound data (byte[SAMPLING_RATE])
 	//
-	public void encByteSound (byte data, byte[] sound, int length) {
-		double freq_s = 200; //[Hz];
-		double freq_0 = 100; //[Hz];
-		double freq_1 = 300; //[Hz];
-		double freq_e = freq_1;
-		
-		int	dL = length / 10;
+	public void encByteSound (byte data, byte[] sound) {
+		double fs = SoundParam.freq_s;
+		double fe = SoundParam.freq_e;
+		double f0 = SoundParam.freq_0;
+		double f1 = SoundParam.freq_1;
+		int L = SoundParam.SAMPLING_RATE;
+		int	dL = SoundParam.NUMBER_OF_SAMPLES;
 		int t = 0;
 		
 		Log.d("encode", "data = " + data);
 
 		// start bit
 		for (int i = 0; i < dL; ++i, ++t) {
-			sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * freq_s / length));
+			sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * fs / L));
 		}
 		for (int bit = 0; bit < 8; ++bit) {
 			Log.d("encode", "[" + bit + "]" + (data >> bit));
 			
 			if (((data >> bit) & 0x01) == 0x01) {
 				for (int i = 0; i < dL; ++i, ++t) {
-					sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * freq_1 / length));
+					sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * f1 / L));
 				}
 			} else {
 				for (int i = 0; i < dL; ++i, ++t) {
-					sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * freq_0 / length));
+					sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * f0 / L));
 				}
 			}
 		}
 		// stop bit
 		for (int i = 0; i < dL; ++i, ++t) {
-			sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * freq_e));
+			sound[t] = (byte) (Byte.MAX_VALUE * Math.sin(2.0 * Math.PI * t * fe / L));
 		}
 	}
 	
---
+++ src/kyLab/droidcoupler/SoundParam.java
@@ -0,0 +1,14 @@
+package kyLab.droidcoupler;
+
+public class SoundParam {
+
+	public static int SAMPLING_RATE = 44100;
+	public static int BPS = 10;
+	public static int NUMBER_OF_SAMPLES = (int) (SAMPLING_RATE * (1.0 / BPS));
+
+	public static double freq_s = 200;		// start bit[Hz]
+	public static double freq_0 = 100;		// binary [Hz]
+	public static double freq_1 = 300;		// binary [Hz]
+	public static double freq_e = freq_1;	// end bit[Hz]
+
+}
